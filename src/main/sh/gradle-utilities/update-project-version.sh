f6e5c424a2513b40cbcb2cdd21271485b5be2a23
Mike Halma <mike.halma@digital.homeoffice.gov.uk>
2019-01-25 13:59:37 +0000

    EE-12943 fix git push command

--- src/main/sh/gradle-utilities/update-project-version.sh
+++ src/main/sh/gradle-utilities/update-project-version.sh
@@ -22,4 +22,4 @@ fi
 ./gradlew release -i -s -Prelease.useAutomaticVersion=true -x runBuildTasks
 NEW_VERSION=$(git describe --abbrev=0)
 sed "/appVersion/c\\appVersion: '${NEW_VERSION}'" Chart.yaml > tempChart.yaml && mv tempChart.yaml Chart.yaml
-git add . && git commit -m "Set chart version to ${NEW_VERSION} [CI SKIP]" && git push
+git add . && git commit -m "Set chart version to ${NEW_VERSION} [CI SKIP]" && git push origin master
